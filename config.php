6f922a1cdf87b3cd7d08ad5bd07f40b202de4714
Nyonya Soekarni <alba.cierr4@gmail.com>
2017-09-27 23:35:05 +0700

    kopet init

---
+++ config.php
@@ -0,0 +1,2 @@
+<?php
+return '{"sitename":"Sempax.v1","perpage":10,"perpagemap":20,"img":{"small":"200x200","medium":"600x600","big":"900x900"},"theme":"aziza","sitename":"Ready","tagline":"Lemper","category_img":{"kitchen":"","bathroom":""},"ads":{"300":"<!--ads-->"},"bing":""}';
